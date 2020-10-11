e81e33fcda268575aee4a93d66dbb0fbebf12964
eliasongun <eongun40@gmail.com>
2020-10-11 13:24:05 +0200

    Test Klasse fertig

---
+++ eongun18_hausuebung3/src/main/java/htlgrieskirchen/pos/dreia/eongun18/Streams.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package htlgrieskirchen.pos.dreia.eongun18;
+
+import java.util.List;
+
+/**
+ *
+ * @author eongu
+ */
+public class Streams {
+    public double average(int[] numbers) {
+        //implement this
+    }
+    
+    public List<String> upperCase(String[] strings) {
+        //implement this
+    }
+    
+    public Weapon findWeaponWithLowestDamage(List<Weapon> weapons) {
+        //implement this
+    }
+    
+    public Weapon findWeaponWithHighestStrength(List<Weapon> weapons) {
+        //implement this
+    }
+    
+    public List<Weapon> collectMissileWeapons(List<Weapon> weapons) {
+        //implement this
+    }
+    
+    public Weapon findWeaponWithLongestName(List<Weapon> weapons) {
+        //implement this
+    }
+    
+    public List<String> toNameList(List<Weapon> weapons) {
+        //implement this
+    }
+    
+    public int[] toSpeedArray(List<Weapon> weapons) {
+        //implement this
+    }
+    
+    public int sumUpValues(List<Weapon> weapons) {
+        //implement this
+    }
+    
+    public long sumUpHashCodes(List<Weapon> weapons) {
+        //implement this
+    }
+    
+    public List<Weapon> removeDuplicates(List<Weapon> weapons) {
+        //implement this
+    }
+    
+    public void increaseValuesByTenPercent(List<Weapon> weapons) {
+       //implement this
+    }
+}
--- eongun18_hausuebung3/src/main/java/htlgrieskirchen/pos/dreia/eongun18/Weapon.java
+++ eongun18_hausuebung3/src/main/java/htlgrieskirchen/pos/dreia/eongun18/Weapon.java
@@ -5,10 +5,120 @@
  */
 package htlgrieskirchen.pos.dreia.eongun18;
 
+import java.util.Objects;
+
 /**
  *
  * @author eongu
  */
-public class Main {
+public class Weapon {
+    
+    private final String name;
+    private final CombatType combatType;
+    private final DamageType damageType;
+    private final int damage;
+    private final int speed;
+    private final int minStrength;
+    private int value;
+
+    public Weapon(String name, CombatType combatType, DamageType damageType, int damage, int speed, int minStrength, int value) {
+        this.name = name;
+        this.combatType = combatType;
+        this.damageType = damageType;
+        this.damage = damage;
+        this.speed = speed;
+        this.minStrength = minStrength;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CombatType getCombatType() {
+        return combatType;
+    }
+
+    public DamageType getDamageType() {
+        return damageType;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getMinStrength() {
+        return minStrength;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.name);
+        hash = 79 * hash + Objects.hashCode(this.combatType);
+        hash = 79 * hash + Objects.hashCode(this.damageType);
+        hash = 79 * hash + this.damage;
+        hash = 79 * hash + this.speed;
+        hash = 79 * hash + this.minStrength;
+        hash = 79 * hash + this.value;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Weapon other = (Weapon) obj;
+        if (this.damage != other.damage) {
+            return false;
+        }
+        if (this.speed != other.speed) {
+            return false;
+        }
+        if (this.minStrength != other.minStrength) {
+            return false;
+        }
+        if (this.value != other.value) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (this.combatType != other.combatType) {
+            return false;
+        }
+        if (this.damageType != other.damageType) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Weapon{" + "name=" + name + ", combatType=" + combatType + ", damageType=" + damageType + ", damage=" + damage + ", speed=" + speed + ", minStrength=" + minStrength + ", value=" + value + '}';
+    }
+    
+    
+    
+    
     
 }
